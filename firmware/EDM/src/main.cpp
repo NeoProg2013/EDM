fe88c2dee9be9d1e14ad2f5a9d676c6c27ec2f86
NeoProg <NeoProg2013@gmail.com>
2026-05-22 14:18:21 +0300

    Changed TFT layout

--- firmware/EDM/src/main.cpp
+++ firmware/EDM/src/main.cpp
@@ -39,7 +39,7 @@ TFT_22_ILI9225 tft(TFT_RST, TFT_RS, TFT_CS, 0, 255);
 HX711 head_tension;
 
 int32_t spark_freq    = 0;
-int32_t spark_t1_us   = 50;
+int32_t spark_t1_us   = 10;
 int32_t spark_t0_us   = 500;
 int32_t spark_current = 0;
 int32_t spark_voltage = 0;
@@ -47,8 +47,12 @@ bool spark_state      = false;
 bool is_short_circuit = false;
 
 
-int32_t feeder_period_ticks = 1000000UL / 100;
-int32_t brake_period_ticks = 1000000UL / 95;
+int32_t feeder_period_us = 1000000UL / 100;
+int32_t brake_period_us = 1000000UL / 95;
+
+#define TENSION_SCALE       (1700) // 1700 bins = 1g
+int32_t tension_bins = 0;
+int32_t tension_g = 0;
 
 
 void keyboard_process() {
@@ -74,72 +78,143 @@ void keyboard_process() {
 }
 
 void update_display() {
-    static bool     is_init = false;
-    static int32_t  last_freq = -1;
-    static int32_t  last_state = -1;
-    static int32_t  last_t1 = -1;
-    static int32_t  last_t0 = -1;
-
-    tft.setFont(Terminal6x8);
-
-    if (!is_init) {
+    static bool s_is_init = false;
+    
+    if (!s_is_init) {
         tft.clear();
         tft.fillRectangle(0, 0, 176, 220, COLOR_BLACK);
+
+        // Static text
+        tft.setFont(Terminal11x16, true);
+        tft.drawText(5, 5, "DISABLED", COLOR_RED);
+        tft.drawLine(0, 27, 176, 27, COLOR_GRAY);
         
+        tft.setFont(Terminal6x8, true);
+        int y = 40;
+        tft.drawText(5, y, "    Freq (Hz): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "Voltage (bin): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, " Current (mA): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "Tension (bin): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "  Tension (g): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "  Feeder (Hz): ---", COLOR_WHITE); y += 15;
+        tft.drawText(5, y, "   Brake (Hz): ---", COLOR_WHITE); y += 15;
+
+        // Draw pulse
         int x = 5;
-        int y = 170;
+        y = 170;
         int w = 160;
         int h = 40;
-
-        // Рисуем линии графика (белым цветом COLOR_WHITE)
         tft.drawLine(x, y, x, y + h, COLOR_WHITE);
         tft.drawLine(x, y, x + w / 2, y, COLOR_WHITE);
         tft.drawLine(x + w / 2, y, x + w / 2, y + h, COLOR_WHITE);
         tft.drawLine(x + w / 2, y + h, x + w, y + h, COLOR_WHITE);
         
-        // Статические подписи, которые не меняются
-        tft.drawText(5, 5, "F:", COLOR_WHITE);
-        // tft.drawText(5, 30, "I: 10000 mA", COLOR_WHITE);
-        // tft.drawText(95, 30, "U: 80 V", COLOR_WHITE);
-        
-        is_init = true;
+        s_is_init = true;
     }
 
-    // Freq
-    if (last_freq != spark_freq) {
-        tft.fillRectangle(23, 5, 90, 15, COLOR_BLACK);
-        tft.drawText(23, 5, String(spark_freq) + " Hz", COLOR_WHITE);
-        last_freq = spark_freq;
-    }
+    tft.setFont(Terminal11x16, true);
 
     // State
-    if (last_state != spark_state) {
-        tft.fillRectangle(5, 20, 90, 30, COLOR_BLACK);
+    static int32_t s_last_state = -1;
+    if (s_last_state != spark_state) {
+        tft.fillRectangle(5, 5, 176, 21, COLOR_BLACK);
         if (is_short_circuit) {
-            tft.drawText(5, 20, "SHORT CIRCUIT", COLOR_RED);
+            tft.drawText(5, 5, "PROTECTION", COLOR_RED);
         } else {
             if (spark_state) {
-                tft.drawText(5, 20, "ENABLED", COLOR_GREEN);
+                tft.drawText(5, 5, "ENABLED", COLOR_GREEN);
             } else {
-                tft.drawText(5, 20, "DISABLED", COLOR_RED);
+                tft.drawText(5, 5, "DISABLED", COLOR_RED);
             }
         }
         
-        last_state = spark_state;
+        s_last_state = spark_state;
     }
 
+    tft.setFont(Terminal6x8, true);
+    int y = 40;
+
+    // Freq
+    static int32_t s_last_freq = -1;
+    if (s_last_freq != spark_freq) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(spark_freq), COLOR_YELLOW);
+        s_last_freq = spark_freq;
+    }
+    y += 15;
+
+    // Voltage
+    static int32_t s_last_voltage = -1;
+    if (s_last_voltage != spark_voltage) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(spark_voltage), COLOR_YELLOW);
+        s_last_voltage = spark_voltage;
+    }
+    y += 15;
+
+    // Current
+    static int32_t s_last_current = -1;
+    if (s_last_current != spark_current) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(spark_current), COLOR_YELLOW);
+        s_last_current = spark_current;
+    }
+    y += 15;
+
+    // Spacing
+    y += 15;
+
+    // Tension (bins)
+    static int32_t s_last_tension_bins = -1;
+    if (s_last_tension_bins != tension_bins) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(tension_bins), COLOR_YELLOW);
+        s_last_tension_bins = tension_bins;
+    }
+    y += 15;
+
+    // Tension (g)
+    static int32_t s_last_tension_g = -1;
+    if (s_last_tension_g != tension_g) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(tension_g), COLOR_YELLOW);
+        s_last_tension_g = tension_g;
+    }
+    y += 15;
+
+    // Feeder freq
+    static int32_t s_last_feeder_period_us = -1;
+    if (s_last_feeder_period_us != feeder_period_us) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(1000000 / feeder_period_us), COLOR_YELLOW);
+        s_last_feeder_period_us = feeder_period_us;
+    }
+    y += 15;
+
+    // Brake freq
+    static int32_t s_last_brake_period_us = -1;
+    if (s_last_brake_period_us != brake_period_us) {
+        tft.fillRectangle(106, y, 150, y + 8, COLOR_BLACK);
+        tft.drawText(106, y, String(1000000 / brake_period_us), COLOR_YELLOW);
+        s_last_brake_period_us = brake_period_us;
+    }
+    y += 15;
+
     // T1
-    if (last_t1 != spark_t1_us) {
+    static int32_t s_last_t1 = -1;
+    if (s_last_t1 != spark_t1_us) {
         tft.fillRectangle(15, 180, 75, 195, COLOR_BLACK);
         tft.drawText(25, 185, String(spark_t1_us) + " us", COLOR_YELLOW);
-        last_t1 = spark_t1_us;
+        s_last_t1 = spark_t1_us;
     }
 
     // T0
-    if (last_t0 != spark_t0_us) {
+    static int32_t s_last_t0 = -1;
+    if (s_last_t0 != spark_t0_us) {
         tft.fillRectangle(100, 180, 160, 195, COLOR_BLACK);
         tft.drawText(105, 185, String(spark_t0_us) + " us", COLOR_YELLOW);
-        last_t0 = spark_t0_us;
+        s_last_t0 = spark_t0_us;
     }
 }
 
@@ -177,6 +252,7 @@ void setup() {
 
     tft.begin();
     tft.setOrientation(2);
+    tft.setBackgroundColor(COLOR_BLACK);
     tft.clear();
     update_display();
 
@@ -204,8 +280,8 @@ void setup() {
     //
     // Setup head feeder
     PWMC_ConfigureChannel(PWM, HEAD_FEEDER_PWM_CH, PWM_CMR_CPRE_CLKA, 0, 0);
-    PWMC_SetPeriod(PWM, HEAD_FEEDER_PWM_CH, feeder_period_ticks);
-    PWMC_SetDutyCycle(PWM, HEAD_FEEDER_PWM_CH, feeder_period_ticks / 2);
+    PWMC_SetPeriod(PWM, HEAD_FEEDER_PWM_CH, feeder_period_us);
+    PWMC_SetDutyCycle(PWM, HEAD_FEEDER_PWM_CH, feeder_period_us / 2);
 
     pinMode(HEAD_FEEDER_EN, OUTPUT);
     pinMode(HEAD_FEEDER_EN, HIGH);
@@ -215,8 +291,8 @@ void setup() {
     //
     // Setup head brake
     PWMC_ConfigureChannel(PWM, HEAD_BRAKE_PWM_CH, PWM_CMR_CPRE_CLKA, 0, 0);
-    PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks);
-    PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks / 2);
+    PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_us);
+    PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_us / 2);
 
     pinMode(HEAD_BRAKE_EN, OUTPUT);
     pinMode(HEAD_BRAKE_EN, HIGH);
@@ -292,7 +368,7 @@ void loop() {
     // Tension control
     static uint32_t s_tension_acc = 0;
     static uint32_t s_tension_acc_n = 0;
-    s_tension_acc += abs(head_tension.get_value());
+    // s_tension_acc += abs(head_tension.get_value());
     s_tension_acc_n++;
 
     static uint32_t s_last_tension_control_ms = 0;
@@ -300,14 +376,18 @@ void loop() {
         if (millis() - s_last_tension_control_ms > 100) {
             int32_t d = 1001000 - s_tension_acc / s_tension_acc_n;
             if (d < -100) {
-                brake_period_ticks = constrain(brake_period_ticks - 10, 2000, 15000);
-                PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks);
-                PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks / 2);
+                brake_period_us = constrain(brake_period_us - 10, 2000, 15000);
+                PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_us);
+                PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_us / 2);
             } else if (d > 100) {
-                brake_period_ticks = constrain(brake_period_ticks + 10, 2000, 15000);
-                PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks);
-                PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_ticks / 2);
+                brake_period_us = constrain(brake_period_us + 10, 2000, 15000);
+                PWMC_SetPeriod(PWM, HEAD_BRAKE_PWM_CH, brake_period_us);
+                PWMC_SetDutyCycle(PWM, HEAD_BRAKE_PWM_CH, brake_period_us / 2);
             }
+
+            tension_bins = s_tension_acc / s_tension_acc_n;
+            tension_g = tension_bins * TENSION_SCALE;
+
             s_last_tension_control_ms = millis();
             s_tension_acc = 0;
             s_tension_acc_n = 0;
